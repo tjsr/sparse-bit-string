706d5ef9b01c3d385318b9ac0d37a41668fa5a68
Tim Rowe <tim@tjsr.id.au>
2023-09-19 19:01:27 +1000

    Fix generating types.

--- src/bitstring.ts
+++ src/bitstring.ts
@@ -2,3 +2,4 @@ export type { EncodedString, CompressedBitArrayWithHeader } from './types';
 export { numberArrayToEncodedString } from './encoder';
 export { extractCompressedBitstring, stringToNumberArray } from './decoder';
 export { generatedCompressedStringWithHeader } from './compactor';
+export { buildSet } from './common';
